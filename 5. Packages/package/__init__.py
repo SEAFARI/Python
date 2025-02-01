1e35d6ecf5b0aed2ee26bb83c5794c3539b2d00f
Seafari <seafari46@gmail.com>
2025-02-01 04:03:56 +0000

    Add Modules, Packages and File handling

---
+++ 5. Packages/package/__init__.py
@@ -0,0 +1,13 @@
+'''__init__.py is a special file used in Python to define
+ packages and initialize their namespaces
+ '''
+ 
+ 
+ 
+ # we need to have this file, even though it is empty
+ 
+ # this file is used to define the package and its namespace
+ 
+ # when we creat this file then only it becomes a package or module
+ 
+ 
---
+++ 5. Packages/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+from package.sub_package.subt import *
+
+print(addition(2,3))
+
+print(sub(20,5))
